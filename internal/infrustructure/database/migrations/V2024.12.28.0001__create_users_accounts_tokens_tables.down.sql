69d158ae7b821664cfb0fcddd28ee0bda0e0b4aa
Mixturka <vadenko23@gmail.com>
2024-12-28 22:29:44 +0300

    feat(migrations): add initial migrations

---
+++ internal/infrustructure/database/migrations/V2024.12.28.0001__create_users_accounts_tokens_tables.down.sql
@@ -0,0 +1,7 @@
+-- V2024.12.28.0001__create_users_accounts_tokens_tables.down.sql
+
+DROP TABLE IF EXISTS tokens;
+
+DROP TABLE IF EXISTS accounts;
+
+DROP TABLE IF EXISTS users;
---
+++ internal/infrustructure/database/migrations/V2024.12.28.0001__create_users_accounts_tokens_tables.up.sql
@@ -0,0 +1,34 @@
+-- V2024.12.28.0001__create_users_accounts_tokens_tables.up.sql
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    profile_picture TEXT DEFAULT '',
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT UNIQUE NOT NULL,
+    is_email_verified BOOLEAN DEFAULT FALSE,
+    is_two_factor_enabled BOOLEAN DEFAULT FALSE,
+    method INTEGER NOT NULL, -- AuthMethod enum (0: Credentials, 1: Google, 2: Yandex)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE accounts (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    type TEXT NOT NULL,
+    provider TEXT NOT NULL,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE, -- Foreign to users
+    refresh_token TEXT,
+    access_token TEXT,
+    expires_at INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE tokens (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_email TEXT NOT NULL,
+    token TEXT UNIQUE NOT NULL,
+    type INTEGER NOT NULL, -- TokenType enum (0: Verification, 1: TwoFactor, 2: PasswordReset)
+    epires_in TIMESTAMP NOT NULL
+)
