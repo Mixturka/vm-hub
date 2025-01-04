a445ab5a4f9d6a15aa13d472fefd5829c04c6658
Mixturka <vadenko23@gmail.com>
2025-01-04 22:31:08 +0300

    test: add build migrate 1

--- scripts/test/test_env_up.sh
+++ scripts/test/test_env_up.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-IMAGE_NAME="migrate"
-
 echo "Starting test database environment..."
 
-if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
-  echo "Image $IMAGE_NAME not found, building..."
-  docker compose -f ./test/docker/docker-compose.yml build migrate
-else
-  echo "Image $IMAGE_NAME found, skipping build..."
-fi
-
-docker compose -f ./test/docker/docker-compose.yml up -d postgres_test
+docker compose -f ./test/docker/docker-compose.yml up --build migrate -d postgres_test
 
 until docker exec vm-hub-postgres-test-db pg_isready -U postgres; do
   echo "Waiting for PostgreSQL..."
