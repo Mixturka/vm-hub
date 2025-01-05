45e044800bfeb3e36f5f04b36e4b7973351664b5
Mixturka <vadenko23@gmail.com>
2025-01-05 19:59:59 +0300

    test: test case

--- internal/app/infrustructure/database/postgres/user_repository_test.go
+++ internal/app/infrustructure/database/postgres/user_repository_test.go
@@ -30,78 +30,78 @@ func prettyLog(t *testing.T, testName string, message string) {
 	t.Logf("==== %s ====\n%s\n", testName, message)
 }
 
-func TestPostgresUserRepository_Save_GetByEmail(t *testing.T) {
-	t.Run("Save User And Get By Email Test", func(t *testing.T) {
-		t.Parallel()
-		prettyLog(t, "TestPostgresUserRepository_Save_GetByEmail", "Starting test to get and save a user")
+// func TestPostgresUserRepository_Save_GetByEmail(t *testing.T) {
+// 	t.Run("Save User And Get By Email Test", func(t *testing.T) {
+// 		t.Parallel()
+// 		prettyLog(t, "TestPostgresUserRepository_Save_GetByEmail", "Starting test to get and save a user")
 
-		user := *test.NewRandomUser()
+// 		user := *test.NewRandomUser()
 
-		prettyLog(t, "TestPostgresUserRepository_Save_GetByEmail", "Inserting user to database")
+// 		prettyLog(t, "TestPostgresUserRepository_Save_GetByEmail", "Inserting user to database")
 
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-		err := repo.Save(ctx, &user)
-		assert.NoError(t, err, "Save shouldn't return an error")
+// 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 		defer cancel()
+// 		err := repo.Save(ctx, &user)
+// 		assert.NoError(t, err, "Save shouldn't return an error")
 
-		fetchedUser, err := repo.GetByEmail(context.Background(), user.Email)
-		assert.NoError(t, err, "GetByEmail shouldn't return an error")
-		assert.NotNil(t, fetchedUser, "Fetched user should not be nil")
-		test.AssertUsersEqual(t, &user, fetchedUser)
+// 		fetchedUser, err := repo.GetByEmail(context.Background(), user.Email)
+// 		assert.NoError(t, err, "GetByEmail shouldn't return an error")
+// 		assert.NotNil(t, fetchedUser, "Fetched user should not be nil")
+// 		test.AssertUsersEqual(t, &user, fetchedUser)
 
-		prettyLog(t, "TestPostgresUserRepository_Save_GetByEmail", "User saved and verified successfully")
-	})
-}
+// 		prettyLog(t, "TestPostgresUserRepository_Save_GetByEmail", "User saved and verified successfully")
+// 	})
+// }
 
-func TestPostgresUserRepository_Save_GetByID(t *testing.T) {
-	t.Run("Save User And Get By ID Test", func(t *testing.T) {
-		t.Parallel()
-		prettyLog(t, "TestPostgresUserRepository_Save_GetByID", "Starting test to save and get a user")
+// func TestPostgresUserRepository_Save_GetByID(t *testing.T) {
+// 	t.Run("Save User And Get By ID Test", func(t *testing.T) {
+// 		t.Parallel()
+// 		prettyLog(t, "TestPostgresUserRepository_Save_GetByID", "Starting test to save and get a user")
 
-		user := *test.NewRandomUser()
+// 		user := *test.NewRandomUser()
 
-		prettyLog(t, "TestPostgresUserRepository_Save_GetByID", "Inserting user to database")
+// 		prettyLog(t, "TestPostgresUserRepository_Save_GetByID", "Inserting user to database")
 
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-		err := repo.Save(ctx, &user)
-		assert.NoError(t, err, "Save shouldn't return an error")
+// 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 		defer cancel()
+// 		err := repo.Save(ctx, &user)
+// 		assert.NoError(t, err, "Save shouldn't return an error")
 
-		fetchedUser, err := repo.GetByID(context.Background(), user.ID)
-		assert.NoError(t, err, "GetByID shouldn't return an error")
-		assert.NotNil(t, fetchedUser, "Fetched user should not be nil")
-		test.AssertUsersEqual(t, &user, fetchedUser)
+// 		fetchedUser, err := repo.GetByID(context.Background(), user.ID)
+// 		assert.NoError(t, err, "GetByID shouldn't return an error")
+// 		assert.NotNil(t, fetchedUser, "Fetched user should not be nil")
+// 		test.AssertUsersEqual(t, &user, fetchedUser)
 
-		prettyLog(t, "TestPostgresUserRepository_Save_GetByID", "User saved and verified successfully")
-	})
-}
+// 		prettyLog(t, "TestPostgresUserRepository_Save_GetByID", "User saved and verified successfully")
+// 	})
+// }
 
-func TestPostgresUserRepository_Save_Update(t *testing.T) {
-	t.Run("Save And Update User Test", func(t *testing.T) {
-		t.Parallel()
-		prettyLog(t, "TestPostgresUserRepository_Save_Update", "Starting test to save and update a user")
+// func TestPostgresUserRepository_Save_Update(t *testing.T) {
+// 	t.Run("Save And Update User Test", func(t *testing.T) {
+// 		t.Parallel()
+// 		prettyLog(t, "TestPostgresUserRepository_Save_Update", "Starting test to save and update a user")
 
-		user := *test.NewRandomUser()
+// 		user := *test.NewRandomUser()
 
-		prettyLog(t, "TestPostgresUserRepository_Save_Update", "Inserting user to database")
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
+// 		prettyLog(t, "TestPostgresUserRepository_Save_Update", "Inserting user to database")
+// 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 		defer cancel()
 
-		err := repo.Save(ctx, &user)
-		assert.NoError(t, err, "Save shouldn't return an error")
+// 		err := repo.Save(ctx, &user)
+// 		assert.NoError(t, err, "Save shouldn't return an error")
 
-		user.Name = "Andrew"
-		err = repo.Update(ctx, &user)
-		assert.NoError(t, err, "Update shouldn't return an error")
+// 		user.Name = "Andrew"
+// 		err = repo.Update(ctx, &user)
+// 		assert.NoError(t, err, "Update shouldn't return an error")
 
-		fetchedUser, err := repo.GetByID(context.Background(), user.ID)
-		assert.NoError(t, err, "GetByID shouldn't return an error")
-		assert.NotNil(t, fetchedUser, "Fetched user should not be nil")
-		test.AssertUsersEqual(t, &user, fetchedUser)
+// 		fetchedUser, err := repo.GetByID(context.Background(), user.ID)
+// 		assert.NoError(t, err, "GetByID shouldn't return an error")
+// 		assert.NotNil(t, fetchedUser, "Fetched user should not be nil")
+// 		test.AssertUsersEqual(t, &user, fetchedUser)
 
-		prettyLog(t, "TestPostgresUserRepository_Save_Update", "User saved and updated successfully")
-	})
-}
+// 		prettyLog(t, "TestPostgresUserRepository_Save_Update", "User saved and updated successfully")
+// 	})
+// }
 
 func TestPostgresUserRepository_Save_Delete(t *testing.T) {
 	t.Run("Save And Delete User Test", func(t *testing.T) {
@@ -126,6 +126,10 @@ func TestPostgresUserRepository_Save_Delete(t *testing.T) {
 		ctx3, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 		defer cancel()
 		fetchedUser, err := repo.GetByID(ctx3, user.ID)
+
+		if fetchedUser == nil {
+			t.Logf("USER NIL")
+		}
 		assert.Error(t, err, "Expected an error when fetching deleted user")
 		assert.Nil(t, fetchedUser, "Fetched user should be nil after deletion")
 	})
